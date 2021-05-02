7606907318a65f7030749a74439a64e8b75036d2
sachio <sachio.engineer@gmail.com>
2021-05-02 12:50:17 +0900

    BASIC認証の実装take3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |name, password|
       name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
+  end
 end
